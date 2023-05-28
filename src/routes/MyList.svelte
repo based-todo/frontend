cc59d70016ea41abcd72a9ac05617724b0a9c322
Al-Andrew <aldea.andrei.977@gmail.com>
2023-05-28 10:31:36 +0300

    svelte

---
+++ src/routes/MyList.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { onMount } from 'svelte';
+
+    let elements = [];
+
+    onMount(async () => {
+        const res = await fetch('http://jsonplaceholder.typicode.com/todos/?_limit=3');
+        elements = await res.json();
+    });
+</script>
+
+<h1>My List</h1>
+
+<ul>
+    {#each elements as element}
+        <li>{element.title}</li>
+    {/each}
+</ul>
